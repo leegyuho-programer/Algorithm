24e02f3b8cd20dd5e8c00a59f402d7791021d763
Lee Gyuho <101032270+leegyuho-programer@users.noreply.github.com>
2024-01-09 00:39:25 +0900

    Time: 476 ms (8.10%), Space: 49.3 MB (17.47%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let answer = [];
+    let maxArr = 0;
+    for(let i = 0; i < s.length; i++) {
+        answer = []; // Initialize the array for each i
+        answer.push(s[i]);
+        for(let j = i+1; j < s.length; j++) {
+            if(!answer.includes(s[j])) { // Check if the character is not in the array
+                answer.push(s[j]);
+            }else {
+                break; // If a duplicate character is found, break the loop
+            }
+        }
+        if(maxArr < answer.length) {
+            maxArr = answer.length; // Update the maximum length
+        }
+    }
+    return maxArr;
+};
