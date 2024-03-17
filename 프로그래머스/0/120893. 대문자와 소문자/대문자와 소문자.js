ae0b950e541c120cda702111e7e0ae23d5e100e8
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-18 00:11:29 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,15 @@
 function solution(my_string) {
-    let result = '';
-    for(let i = 0; i < my_string.length; i++) {
-        if(my_string[i] === my_string[i].toUpperCase()) {
-            result += my_string[i].toLowerCase();
-        } else {
-            result += my_string[i].toUpperCase();
-        }
-    }
-    return result;
+    return [...my_string].map((e) => e === e.toUpperCase() ? e.toLowerCase() : e.toUpperCase()).join('');
 }
+
+// function solution(my_string) {
+//     let result = '';
+//     for(let i = 0; i < my_string.length; i++) {
+//         if(my_string[i] === my_string[i].toUpperCase()) {
+//             result += my_string[i].toLowerCase();
+//         } else {
+//             result += my_string[i].toUpperCase();
+//         }
+//     }
+//     return result;
+// }
