96f7f522b15039d42f006cec109dab63d1453167
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-21 11:59:08 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -2,7 +2,7 @@ function solution(n) {
     let nextN = n + 1;
     
     while(true) {
-        if(nextN.toString(2).split('').filter(e => e === '1').length === n.toString(2).split('').filter(e => e === '1').length && nextN > n) {
+        if(nextN.toString(2).split('').filter(e => e === '1').length === n.toString(2).split('').filter(e => e === '1').length) {
             break;
         }
         nextN++;
