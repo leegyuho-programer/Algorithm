a33985edd9ab3e272affba13343742f3647b5c27
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-22 16:52:32 +0900

    Update 올바른 괄호.js

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,3 +1,20 @@
+// 첫번째 풀이
+function solution(s){
+    let left = 0;
+    let right = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === '(') {
+            left++;
+        } else {
+            right++;
+        }
+        if(right > left) return false;
+    }
+    return s[0] === '(' && s[s.length - 1] === ')' && left === right ? true : false;
+}
+
+// 두번째 풀이
 function solution(s){
     let result = 0;
     
