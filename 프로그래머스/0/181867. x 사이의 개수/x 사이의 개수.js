b462193feb7a81ee02798746a0ee82a2189b3f19
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-25 13:38:54 +0900

    [level 0] Title: x 사이의 개수, Time: 3.04 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -0,0 +1,9 @@
+function solution(myString) {
+    myString = myString.split('x');
+    let result = [];
+    
+    for(let i = 0; i < myString.length; i++) {
+        result.push(myString[i].length);
+    }
+    return result;
+}
