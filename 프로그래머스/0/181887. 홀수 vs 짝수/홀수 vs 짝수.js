212e614f6b636be6780ba6a1951b44031b63dab8
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-25 19:03:03 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -2,12 +2,21 @@ function solution(num_list) {
     let odd = 0;
     let even = 0;
     
-    for(let i = 0; i < num_list.length; i++) {
-        if(i % 2 === 0) {
-            odd += num_list[i];
-        } else {
-            even += num_list[i];
-        }
-    }
+    num_list.forEach((e, idx) => (idx%2) === 0 ? odd += e : even += e);
+    
     return Math.max(odd, even);
 }
+
+// function solution(num_list) {
+//     let odd = 0;
+//     let even = 0;
+    
+//     for(let i = 0; i < num_list.length; i++) {
+//         if(i % 2 === 0) {
+//             odd += num_list[i];
+//         } else {
+//             even += num_list[i];
+//         }
+//     }
+//     return Math.max(odd, even);
+// }
