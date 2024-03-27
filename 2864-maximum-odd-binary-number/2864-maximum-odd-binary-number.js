af213665b27464a22fff37aa6b652f408c6fa5cd
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-27 20:45:28 +0900

    Time: 84 ms (22.38%), Space: 53.5 MB (8.97%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.js
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.js
@@ -3,8 +3,5 @@
  * @return {string}
  */
 var maximumOddBinaryNumber = function(s) {
-    let arr = [...s].sort((a,b) => (b-a)).slice(1, s.length);
-    console.log(arr)
-
-    return arr.join('') + '1'
+    return [...s].sort((a,b) => (b-a)).slice(1, s.length).join('') + '1'
 };
