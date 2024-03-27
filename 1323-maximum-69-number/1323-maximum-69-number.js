420b5076069037d7782f0d67f7168da63e037a8e
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-27 21:04:30 +0900

    Time: 50 ms (69.50%), Space: 48.5 MB (83.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    num = num.toString().split('');
-
-    for(let i = 0; i < num.length; i++) {
-        if(num[i] === '6') {
-            num[i] = '9';
-            break;
-        } 
-    }
-    return num.join('')
+    return num.toString().replace('6', '9')
 };
