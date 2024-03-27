8a48646e57ba0f8cdc6c590ebc1749104db3ff3d
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-27 15:06:00 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,10 +1,14 @@
 function solution(todo_list, finished) {
-    let result = [];
-    
-    for(let i = 0; i < todo_list.length; i++) {
-        if(finished[i] === false) {
-            result.push(todo_list[i])
-        }
-    }
-    return result;
+    return todo_list.filter((e, idx) => finished[idx] === false);
 }
+
+// function solution(todo_list, finished) {
+//     let result = [];
+    
+//     for(let i = 0; i < todo_list.length; i++) {
+//         if(finished[i] === false) {
+//             result.push(todo_list[i])
+//         }
+//     }
+//     return result;
+// }
