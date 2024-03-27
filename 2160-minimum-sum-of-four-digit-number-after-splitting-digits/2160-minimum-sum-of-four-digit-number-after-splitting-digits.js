c2984ad8819faff1cbf212145d47c11b5c700639
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-27 19:47:38 +0900

    Time: 59 ms (15.96%), Space: 48.6 MB (72.07%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -3,10 +3,6 @@
  * @return {number}
  */
 var minimumSum = function(num) {
-    num = num.toString();
-    num = [...num].map(e => Number(e));
-    num.sort((a, b) => a - b);
-    num1 = num[0].toString() + num[2].toString()
-    num2 = num[1].toString() + num[3].toString()
-    return Number(num1) + Number(num2);
+    num = num.toString().split('').sort();
+    return Number(num[0] + num[2]) + Number(num[1] + num[3])
 };
