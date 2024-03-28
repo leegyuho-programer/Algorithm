2034b7f8cc1e2eea63fda1e3d8d7a38b4a4bf73f
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-28 16:10:29 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(my_strings, parts) {
+    let result = '';
+    
+    for(let i = 0; i < my_strings.length; i++) {
+        result += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
+    }
+    return result;
+}
