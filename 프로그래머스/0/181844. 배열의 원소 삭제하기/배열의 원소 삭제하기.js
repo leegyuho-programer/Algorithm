cba4425f548331420dfdcff45a7342499bf74ad8
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-28 17:01:54 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,10 +1,14 @@
 function solution(arr, delete_list) {
-    let result = []
-    
-    for(let i = 0; i < arr.length; i++) {
-        if(!delete_list.includes(arr[i])) {
-            result.push(arr[i])   
-        }
-    }
-    return result;
+    return arr.filter(e => !delete_list.includes(e))
 }
+
+// function solution(arr, delete_list) {
+//     let result = []
+    
+//     for(let i = 0; i < arr.length; i++) {
+//         if(!delete_list.includes(arr[i])) {
+//             result.push(arr[i])   
+//         }
+//     }
+//     return result;
+// }
