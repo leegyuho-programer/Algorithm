793e52fad40e1e59ac753bc11aac83b0f5ef2ea8
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-28 17:17:31 +0900

    [level 0] Title: 주사위의 개수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,8 +1,12 @@
 function solution(box, n) {
-    let result = 1;
-    
-    for(let i = 0; i < box.length; i++) {
-        result *= Math.floor(box[i] / n);
-    }
-    return result;
+    return box.reduce((cur, prev) => cur * Math.floor(prev/n), 1)
 }
+
+// function solution(box, n) {
+//     let result = 1;
+    
+//     for(let i = 0; i < box.length; i++) {
+//         result *= Math.floor(box[i] / n);
+//     }
+//     return result;
+// }
