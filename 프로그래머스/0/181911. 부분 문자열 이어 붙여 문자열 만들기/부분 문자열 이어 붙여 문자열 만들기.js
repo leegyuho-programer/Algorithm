bd9b1f515fb67750698807d152f356c8a41c50e2
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-28 16:18:38 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -1,8 +1,12 @@
 function solution(my_strings, parts) {
-    let result = '';
-    
-    for(let i = 0; i < my_strings.length; i++) {
-        result += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
-    }
-    return result;
+    return my_strings.map((e, idx) => e.slice(parts[idx][0], parts[idx][1] + 1)).join('')
 }
+
+// function solution(my_strings, parts) {
+//     let result = '';
+    
+//     for(let i = 0; i < my_strings.length; i++) {
+//         result += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
+//     }
+//     return result;
+// }
