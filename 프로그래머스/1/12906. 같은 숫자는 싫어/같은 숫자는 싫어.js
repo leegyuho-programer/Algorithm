bc6c3cb28ebe5df9d1b4c647107b74d315bda16a
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-03-30 12:25:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 16.22 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -3,10 +3,6 @@ function solution(arr){
     let stack = [];
     
     for(let i = 0; i < arr.length; i++) {
-        // if(result.length === 0) {
-        //     result.push(arr[i])
-        //     stack.push(arr[i]);
-        // } 
         if(stack[0] === arr[i]) {
             continue;
         } else {
