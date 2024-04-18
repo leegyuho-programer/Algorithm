087bf0e8668da45534bd97d8a8ec5e541a5d104b
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-04-19 00:21:36 +0900

    Update 스킬트리.js

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -12,3 +12,7 @@ function solution(skill, skill_trees) {
     }
     return result;
 }
+
+// 처음에는 join을 사용하지 않고 [...skill]을 사용하여 skill도 배열로 만들고
+// skill_trees[i]와 sk가 같은지 비교했는데 [C, B, D] === [C, B, D] 이렇게 해도 
+// 배열은 참조타입이기 때문에 일치하지 않아서 join을 사용하여 문자열로 만든 후 일치 연산자를 사용하여 비교했다.
