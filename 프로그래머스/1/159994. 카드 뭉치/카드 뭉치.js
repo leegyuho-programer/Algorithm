ea36eeab9699e409bc9e306a1dc29bc6760459b0
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-04-22 13:53:17 +0900

    Update 카드 뭉치.js

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,6 +1,4 @@
 function solution(cards1, cards2, goal) {
-    let result;
-    
     let a = goal.filter(e => cards1.includes(e));
     let b = goal.filter(e => cards2.includes(e));
     
@@ -25,7 +23,7 @@ function solution(cards1, cards2, goal) {
 // } 
 
 
-// 두 번째 풀이 - 테스트 25 실패
+// 두 번째 풀이 - 테스트 25 실패(카드를 사용하지 않고 다음 카드로 넘어갈 수 없습니다.)
 
 // function solution(cards1, cards2, goal) {
 //     let a = goal.filter(e => cards1.includes(e));
@@ -33,7 +31,7 @@ function solution(cards1, cards2, goal) {
     
 //     cards1 = cards1.filter(e => goal.includes(e));
 //     cards2 = cards2.filter(e => goal.includes(e));
-//     //5-6번째 줄은 cards1이나 cards2 안에 있는 것이 goal에 없을 수 있기 때문에 작성
+//     //위에 두줄은 cards1이나 cards2 안에 있는 것이 goal에 없을 수 있기 때문에 작성(테스트 20, 21, 24)
 
 //     if(a.join('') === cards1.join('') && b.join('') === cards2.join('')) return 'Yes';
 //     return 'No'
