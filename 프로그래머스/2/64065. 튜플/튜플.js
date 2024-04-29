9d8a4daffadbbea7f20e32ff731d58a3682315d9
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-04-29 13:35:12 +0900

    [level 2] Title: 튜플, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,4 +1,17 @@
 function solution(s) {
-    var answer = [];
-    return answer;
+    let result = [];
+    let newArr = s.replace('{{', '').replace('}}', '').split('},{');
+    newArr.sort((a,b) => a.length - b.length);
+    
+    for(let i = 0; i < newArr.length; i++) {
+        let tuple = newArr[i].split(',');
+        for(let j = 0; j < tuple.length; j++) {
+            let num = parseInt(tuple[j]);
+            
+                result.push(num);
+            
+        }
+    }
+    result = [...new Set(result)]
+    return result;
 }
