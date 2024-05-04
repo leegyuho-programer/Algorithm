65ba0f051a4c9972a91cf993ef396887ce8b77b5
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-05-04 23:07:24 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,9 @@
+function solution(A, B) {
+    for (let i = 0; i < A.length; i++) {
+        if (A === B) {
+            return i;
+        }
+        A = A.slice(A.length-1) + A.slice(0, A.length-1);
+    }
+    return -1;
+}
