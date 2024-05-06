6c16b5cf523c3a0be397f49a1e3dbfee0c681c8d
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-05-06 14:12:23 +0900

    Time: 143 ms (5.12%), Space: 57.7 MB (5.95%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -7,7 +7,7 @@ var largestNumber = function(nums) {
         let newA = a.toString()
         let newB = b.toString()
         console.log(Number(newA + newB) > Number(newB + newA) ? -1 : 1)
-        return Number(newA + newB) > Number(newB + newA) ? -1 : 1
+        return Number(newB + newA) - Number(newA + newB)
     });
     if(nums[0] === 0) return '0';
 
