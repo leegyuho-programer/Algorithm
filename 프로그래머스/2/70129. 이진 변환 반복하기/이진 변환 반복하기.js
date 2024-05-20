29252659fb180ae9cc7d03dc09aef0866e9d757a
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-05-20 13:14:41 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.03 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,12 +1,13 @@
 function solution(s) {
     let cnt = 0;
     let zero = 0;
+    
     while(s !== '1') {
-      let sLen = s.length
-      s = s.replace(/0/g, ''); //0을 제거
-      zero += sLen - s.length;
-      cnt ++;
-      s = s.length.toString(2);
+        let SLen = s.length;
+        s = s.replace(/0/g, '');
+        zero += SLen - s.length;
+        cnt ++;
+        s = s.length.toString(2);
     }
-    return [cnt, zero]
+    return [cnt, zero];
 }
