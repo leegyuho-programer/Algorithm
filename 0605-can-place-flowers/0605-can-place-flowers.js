385eb316fca1dbb68613032160fd84afd7e42f7f
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-05-26 23:22:55 +0900

    Time: 58 ms (59.15%), Space: 51.4 MB (25.41%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -7,14 +7,13 @@ var canPlaceFlowers = function(flowerbed, n) {
     let cnt = 0;
 
     for (let i = 0; i < flowerbed.length; i++) {
-        if (flowerbed[i] === 0) {
-            let prev = (i === 0) ? 0 : flowerbed[i - 1];
-            let next = (i === flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+        let prev = (i === 0) ? 0 : flowerbed[i-1];
+        let cur = flowerbed[i]
+        let next = (i === flowerbed.length - 1) ? 0 : flowerbed[i+1];
 
-            if (prev === 0 && next === 0) {
-                flowerbed[i] = 1;
-                cnt++;
-            }
+        if(prev === 0 && cur === 0 && next === 0) {
+            cnt++;
+            flowerbed[i] = 1;
         }
     }
 
