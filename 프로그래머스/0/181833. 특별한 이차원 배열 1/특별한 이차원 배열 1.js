e7810a2a2788d6d891dd1ec4209123efcb9f04cb
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-06-29 09:56:27 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.10 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = [];
+
+    for(let i = 0; i < n; i++) {
+        let arr = Array(n).fill(0)
+        arr[i] = 1;
+        answer.push(arr)
+    }
+    return answer;
+}
