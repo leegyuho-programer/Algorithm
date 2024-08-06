e8158a250ac44e38fb37c231af566f4780f26133
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-08-07 01:24:56 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.20 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    let result = '';
+    s = s.split(' ')
+    
+    for(let i = 0; i < s.length; i++) {
+        s[i] = Number(s[i])
+    }
+    
+    s.sort((a,b) => a - b);
+    
+    result += (s[0]).toString();
+    result += ' ' + (s[s.length - 1]).toString();
+
+    return result;
+}
