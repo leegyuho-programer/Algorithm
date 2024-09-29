b83c2842dbf70691a838493882ec103616ee1e05
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-09-29 18:00:39 +0900

    [level 2] Title: 짝지어 제거하기, Time: 55.54 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,11 +2,11 @@ function solution(s) {
     let stack = [];
     
     for(let i = 0; i < s.length; i++) {
-        if(stack.length === 0 || stack[stack.length-1] !== s[i]) {
-            stack.push(s[i])
+        if(stack.length === 0 || stack[stack.length - 1] !== s[i]) {
+            stack.push(s[i]);
         } else {
             stack.pop()
         }
     }
-    return stack.length === 0 ? 1 : 0
+    return stack.length ? 0 : 1;
 }
