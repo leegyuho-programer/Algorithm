1088968f528ec750703521af2cb24f6756e28316
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-09-29 15:52:22 +0900

    [level 2] Title: 피보나치 수, Time: 5.22 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,9 @@
 function solution(n) {
-    let fibonacci = [0, 1];
+    let result = [0, 1];
     
-    for(let i = 2; i <= n; i++){
-        fibonacci.push(fibonacci[i] = (fibonacci[i-2] + fibonacci[i-1]) % 1234567);
+    for(let i = 2; i <= n; i++) {
+        result[i] = (result[i-1] + result[i-2]) % 1234567;
     }
-    return fibonacci[n];
     
+    return result[n]
 }
