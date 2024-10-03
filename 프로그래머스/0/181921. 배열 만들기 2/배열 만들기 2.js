51087b088581aa3f5afbb6968086f2cb7220cb67
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-10-03 17:55:20 +0900

    [level 0] Title: 배열 만들기 2, Time: 121.97 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -2,9 +2,8 @@ function solution(l, r) {
     let result = [];
     
     for(let i = l; i <= r; i++) {
-        if(i.toString().split('').every(num => num === '0' || num === '5')) {
-                result.push(i);
-        }
-    } 
-    return result.length ? result : [-1]
+        let str = i.toString();
+        if([...str].every(char => char === '0' || char === '5')) result.push(parseInt(str));
+    }
+    return result.length ? result : [-1];
 }
