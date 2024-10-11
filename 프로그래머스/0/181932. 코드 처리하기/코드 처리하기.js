615f18995465c7c11cef83bdc7b63c52b8482e5b
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-10-11 11:44:37 +0900

    [level 0] Title: 코드 처리하기, Time: 38.02 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,25 +1,21 @@
 function solution(code) {
-    let result = '';
     let mode = 0;
+    let result = '';
     
     for(let i = 0; i < code.length; i++) {
         if(mode === 0) {
-                if(code[i] !== '1') {
-                    if(i % 2 === 0) {
-                        result += code[i];   
-                    }
-                } else {
-                    mode = 1;
-                }
-        } else {
-                if(code[i] !== '1') {
-                    if(i % 2 !== 0) {
-                        result += code[i];   
-                    }
-                } else {
-                    mode = 0;
-                }
+            if(code[i] !== '1' && i % 2 === 0) {
+                result += code[i];
+            } else if(code[i] === '1') {
+                mode = 1;
+            }
+        } else if(mode === 1) {
+            if(code[i] !== '1' && i % 2 !== 0) {
+                result += code[i];
+            } else if(code[i] === '1') {
+                mode = 0;
+            }
         }
     }
-    return result ? result : 'EMPTY'
+    return result.length ? result : 'EMPTY'
 }
