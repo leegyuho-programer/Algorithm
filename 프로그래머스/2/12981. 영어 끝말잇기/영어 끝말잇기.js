b5f6946dbccc1ebbd0a8e5e50784d87c5c4f5d0e
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-10-23 01:12:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -2,13 +2,9 @@ function solution(n, words) {
     let stack = [words[0]];
     
     for(let i = 1; i < words.length; i++) {
-        if(stack.includes(words[i])) {
+        if(stack.includes(words[i]) || stack[stack.length - 1][stack[stack.length - 1].length - 1] !== words[i][0]) {
             return [i % n + 1, Math.ceil((i + 1) / n)]
-        }
-        
-        if(stack[stack.length - 1][stack[stack.length - 1].length - 1] !== words[i][0]) {
-            return [i % n + 1, Math.ceil((i + 1) / n)]
-        } else {
+        }  else {
             stack.push(words[i]);
         }
     }
