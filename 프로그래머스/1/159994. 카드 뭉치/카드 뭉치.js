a2dca58ed243ef46272566637b6c902f06bed5e5
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-10-30 19:15:15 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -4,10 +4,8 @@ function solution(cards1, cards2, goal) {
     
     for(let word of goal) {
         if(idx1 < cards1.length && cards1[idx1] === word) {
-            console.log('1', word)
             idx1++;
         } else if(idx2 < cards2.length && cards2[idx2] === word) {
-            console.log('2', word)
             idx2++;
         } else {
             return 'No'
@@ -15,13 +13,3 @@ function solution(cards1, cards2, goal) {
     }
     return 'Yes'
 }
-
-// function solution(cards1, cards2, goal) {
-//     let goalCards1 = goal.filter(e => !cards2.includes(e));
-//     let goalCards2 = goal.filter(e => !cards1.includes(e));
-//     cards1 = cards1.filter(e => goalCards1.includes(e));
-//     cards2 = cards2.filter(e => goalCards2.includes(e));
-    
-//     if(goalCards1.join('') === cards1.join('') && goalCards2.join('') === cards2.join('')) return 'Yes'
-//     else return 'No'
-// }
