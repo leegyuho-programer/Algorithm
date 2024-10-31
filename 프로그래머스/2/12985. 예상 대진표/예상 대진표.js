914b9f2ba71f0979a959c935be4f50ce7f101a7b
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-10-31 09:31:42 +0900

    [level 2] Title: 예상 대진표, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,20 +1,13 @@
 function solution(n,a,b) {
     let cnt = 0;
     
-    for(let i = 0; i < n; i++) {
+    for(let i = 0; i < Math.log2(n); i++) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        cnt++;
+        
         if(a === b) {
             return cnt;
-        } else {
-            a = Math.ceil(a / 2);
-            b = Math.ceil(b / 2);
-            cnt++;
         }
     }
 }
-
-// 2 / 6
-
-// 1 2   3 4   5 6   7 8
-// 2       4     6     8
-//     2            6
-// 3번
