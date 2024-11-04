844073337067d1928351a02ed825c9895c29241c
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-11-04 15:47:06 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1069.19 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,13 +1,28 @@
+// function solution(elements) {
+//     let set = new Set();
+//     let arr = [...elements, ...elements]
+//     console.log(arr)
+    
+//     for(let i = 1; i <= elements.length; i++) {
+//         for(let j = 0; j < elements.length; j++) {
+//             set.add(arr.slice(j, j+i).reduce((a,b) => a + b, 0));
+//         }
+//     }
+
+//     return set.size;
+// }
+
+
 function solution(elements) {
-    let set = new Set();
-    let arr = [...elements, ...elements.slice(0, elements.length - 1)]
+    let result = [];
+    let arr = [...elements, ...elements]
     
     for(let i = 1; i <= elements.length; i++) {
         for(let j = 0; j < elements.length; j++) {
-            set.add(arr.slice(j, j+i).reduce((a,b) => a + b, 0));
+            result.push(arr.slice(j, j+i).reduce((prev, cur) => (prev + cur), 0))
         }
     }
-
-    return set.size;
+    let set = new Set(result)
+    return set.size
 }
 
