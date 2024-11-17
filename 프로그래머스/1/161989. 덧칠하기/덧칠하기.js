733a55ae0056f6611cbd4d39bcce741cd42b7926
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-11-17 18:26:23 +0900

    [level 1] Title: 덧칠하기, Time: 5.16 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,25 +1,13 @@
 function solution(n, m, sections) {
-    let answer = 0;
+    let result = 0;
     let painted = 0;
+    
     for(let section of sections) {
         if(painted < section) {
-            answer++;
-            painted = section+m-1;
+            result++;
+            painted = section + m - 1;
         }
     }
-    return answer;
-}
-
-// function solution(n, m, section) {
-//     let result = 1;
     
-//     for(let i = 0; i < section.length; i++) {
-//         for(let j = i+1; j < section.length; j++) {
-//             if(Math.abs(section[i] - section[j]) >= m) {
-//                 result++;
-//                 break;
-//             }
-//         }
-//     }
-//     return result;
-// }
+    return result;
+}
