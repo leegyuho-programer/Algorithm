b2ba8284cc73a3987f4fe00ed369f48997678284
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-11-28 15:42:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 23.55 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,26 +1,26 @@
 function solution(maps) {
-    let n = maps.length; // 행 갯수
-    let m = maps[0].length; // 열 갯수
-    let queue = [[0, 0, 1]]; // x, y, 거리
-    let directions = [
+    const directions = [
         [0, 1], // 오른쪽
         [1, 0], // 아래쪽
         [0, -1], // 왼쪽
-        [-1, 0], // 위쪽
+        [-1, 0], // 윗쪽
     ];
+    const n = maps.length;
+    const m = maps[0].length;
+    let queue = [[0, 0, 1]];
     
     while(queue.length > 0) {
-        const [x, y, dist] = queue.shift(); // 현재 위치 가져오기
+        const [x, y, dist] = queue.shift()
         
         if(x === n - 1 && y === m - 1) return dist;
         
         for(const [dx, dy] of directions) {
-            const nx = x + dx;
-            const ny = y + dy;
+            const nx = dx + x;
+            const ny = dy + y;
             
             if(nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] === 1) {
-                maps[nx][ny] = 0;
                 queue.push([nx, ny, dist + 1]);
+                maps[nx][ny] = 0;
             }
         }
     }
