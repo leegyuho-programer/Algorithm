dcd13989c052b8c6440450013a1a829c91e446fb
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-12-04 20:01:19 +0900

    [level 2] Title: [3차] 압축, Time: 0.99 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,31 @@
+function solution(msg) {
+    let dictionary = {};
+    let lastIndex = 26;
+    let result = [];
+    
+    for(let i = 1; i <= 26; i++) {
+        dictionary[String.fromCharCode(i + 64)] = i;
+    }
+    
+    let i = 0;
+    while(msg.length > i) {
+        let word = msg[i];
+        let j = i + 1;
+        
+        while(msg.length >= j && dictionary[word]) {
+            if(msg.length === j) break;
+            let nextWord = word + msg[j];
+            if(!dictionary[nextWord]) break;
+            word = nextWord;
+            j++;
+        }
+        
+        result.push(dictionary[word]);
+        
+        if(msg.length > j) dictionary[word + msg[j]] = ++lastIndex;
+        
+        i += word.length;
+    }
+    
+    return result;
+}
