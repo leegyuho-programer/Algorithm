0000ebbe392710593fdf8b449c64f147619e6365
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-12-19 16:27:30 +0900

    [level 2] Title: 스킬트리, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -2,15 +2,9 @@ function solution(skill, skill_trees) {
     let result = 0;
     
     for(let i = 0; i < skill_trees.length; i++) {
-        let cnt = 0;
-        
         skill_trees[i] = [...skill_trees[i]].filter(e => skill.includes(e)).join('');
         
-        for(let j = 0; j < skill_trees[i].length; j++) {
-            if(skill[j] === skill_trees[i][j]) cnt++;
-        }
-        
-        if(skill_trees[i].length === cnt) result++;
+        if(skill_trees[i] === skill.substr(0, skill_trees[i].length)) result++;
     }
 
     return result;
