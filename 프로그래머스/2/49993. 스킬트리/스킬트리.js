9a9d62ed9689bde24b1623d715cd24d5aae2c747
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2024-12-19 16:25:13 +0900

    [level 2] Title: 스킬트리, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -2,17 +2,16 @@ function solution(skill, skill_trees) {
     let result = 0;
     
     for(let i = 0; i < skill_trees.length; i++) {
-        skill_trees[i] = [...skill_trees[i]].filter(e => skill.includes(e)).join('')
-        console.log(skill_trees[i], i)
-        let sk = skill.substring(0, skill_trees[i].length);
-        console.log(sk, i)
-        if(skill_trees[i] === sk) {
-            result++;
+        let cnt = 0;
+        
+        skill_trees[i] = [...skill_trees[i]].filter(e => skill.includes(e)).join('');
+        
+        for(let j = 0; j < skill_trees[i].length; j++) {
+            if(skill[j] === skill_trees[i][j]) cnt++;
         }
+        
+        if(skill_trees[i].length === cnt) result++;
     }
+
     return result;
 }
-
-// 처음에는 join을 사용하지 않고 [...skill]을 사용하여 skill도 배열로 만들고
-// skill_trees[i]와 sk가 같은지 비교했는데 [C, B, D] === [C, B, D] 이렇게 해도 
-// 배열은 참조타입이기 때문에 일치하지 않아서 join을 사용하여 문자열로 만든 후 일치 연산자를 사용하여 비교했다.
