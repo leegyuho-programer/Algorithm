4e871e2588e8d82ac826922db62a8de6759ccc9a
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-01-08 14:18:06 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,11 +1,12 @@
 function solution(new_id) {
-    new_id = new_id.toLowerCase().replace(/[^a-z0-9._-]/g, '').replace(/\.{2,}/g, '.').replace(/^\.+|\.+$/g, '');
+    new_id = new_id.toLowerCase()
+                   .replace(/[^a-z0-9._-]/g, '')
+                   .replace(/\.{2,}/g, '.')
+                   .replace(/^\.+|\.+$/g, '')
+                   .substr(0, 15)
+                   .replace(/^\.+|\.+$/g, '');
     
     if(new_id.length === 0) new_id = 'a';
-    
-    if(new_id.length >= 16) new_id = new_id.substr(0, 15);
-    
-    if(new_id[new_id.length - 1] === '.') new_id = new_id.replace(/^\.+|\.+$/g, '');
     
     while(new_id.length <= 2) {
         new_id += new_id[new_id.length - 1];
