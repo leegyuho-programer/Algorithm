ee36d14817f484de4cbbccf56a5c73929405a5f3
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-01-23 16:04:39 +0900

    [level 2] Title: 주차 요금 계산, Time: 4.20 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -2,56 +2,47 @@ function solution(fees, records) {
     let result = [];
     let dict = new Map();
     
-    const lastOutTime = 23 * 60 + 59; // 23시 59분까지 출차가 되지 않은 경우
-    const [minimumFareTimePerMinute, minimumFare, extraPostageTimePerMinute, extraPostage] = fees;
+    const lastOutTime = 23 * 60 + 59;
+    const [minimumTime, minimumFee, extraTime, extraFee] = fees;
     
     records = records.map(record => {
         const [time, car, type] = record.split(' ');
         const [hour, minute] = time.split(':').map(Number);
+        
         return [hour * 60 + minute, car, type];
-    });
+    })
     
-    // 차량별 입출차 기록 및 주차 시간 누적
-    for(const [time, car, type] of records) {
-        if(!dict.has(car)) {
-            dict.set(car, { total: 0, lastIn: null});
-        }
+    for(let [time, car, type] of records) {
+        if(!dict.has(car)) dict.set(car, { total: 0, lastIn: null });
         
         const carData = dict.get(car);
         
         if(type === 'IN') {
-            carData.lastIn = time; // 입차 시간 기록
+            carData.lastIn = time;
         } else {
-            carData.total += time - carData.lastIn; // 누적 주차 시간 계산
-            carData.lastIn = null; // 출차 처리
+            carData.total += time - carData.lastIn;
+            carData.lastIn = null;
         }
     }
     
-    // 출차 기록이 없는 차량 처리
-    for(const [car, carData] of dict) {
+    for(let [car, carData] of dict) {
         if(carData.lastIn !== null) {
             carData.total += lastOutTime - carData.lastIn;
             carData.lastIn = null;
         }
     }
     
-    // 요금 계산
     [...dict]
         .sort((a, b) => a[0] - b[0])
         .forEach(e => {
         const car = e[0];
-        const total = e[1].total;
+        const carData = e[1];
         
-        let fee;
-        
-        if(total <= minimumFareTimePerMinute) {
-            fee = minimumFare;
+        if(carData.total > minimumTime) {
+            result.push(minimumFee + Math.ceil((carData.total - minimumTime) / extraTime) * extraFee);
         } else {
-            const extraTime = total - minimumFareTimePerMinute;
-            fee = minimumFare + Math.ceil(extraTime / extraPostageTimePerMinute) * extraPostage;
+            result.push(minimumFee);
         }
-        
-        result.push(fee);
     })
     
     return result;
