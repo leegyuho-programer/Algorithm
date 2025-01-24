66c1e04813fe0f437a3c1a68a1679f550270b8a9
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-01-24 10:33:16 +0900

    [level 2] Title: 숫자 변환하기, Time: 130.32 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,28 +1,28 @@
 function solution(x, y, n) {
-    if (x === y) return 0;
-
-    let queue = new Set([x]);
-    let visited = new Set([x]);
+    if(x === y) return 0;
+    
     let cnt = 0;
-
-    while (queue.size > 0) {
-        const nextQueue = new Set();
-
-        for (const current of queue) {
-            const nextValues = [current + n, current * 2, current * 3];
-
-            for (const next of nextValues) {
-                if (next > y || visited.has(next)) continue;
-                if (next === y) return cnt + 1;
-
+    let temp = new Set([x]);
+    let visited = new Set([x]);
+    
+    while(temp.size > 0) {
+        let nextTemp = new Set();
+        
+        for(const current of temp) {
+            const nextX = [current + n, current * 3, current * 2];
+            
+            for(const next of nextX) {
+                if(next > y || visited.has(next)) continue;
+                if(next === y) return cnt + 1;
+                
+                nextTemp.add(next);
                 visited.add(next);
-                nextQueue.add(next);
             }
         }
-
-        queue = nextQueue;
+        
+        temp = nextTemp;
         cnt++;
     }
-
+    
     return -1;
 }
