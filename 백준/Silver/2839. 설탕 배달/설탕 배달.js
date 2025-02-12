989d1dc03027930a0244bc8fae4295f6c1a39766
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-02-12 14:24:03 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+
+let count = 0;
+
+while(true) {
+    if(input % 5 === 0) {
+        console.log(input / 5 + count);
+        break;
+    }
+    
+    if(0 > input) {
+        console.log(-1);
+        break;
+    }
+    
+    count++;
+    input -= 3;
+}
