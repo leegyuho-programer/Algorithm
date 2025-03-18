2d1e8ee9e13d97b0233846c1e5ddbb99551eaa61
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-03-18 20:01:39 +0900

    [Silver I] Title: 양치기 꿍, Time: 252 ms, Memory: 17156 KB -BaekjoonHub

---
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+let input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.trim());
+
+const [R, C] = input[0].split(' ').map(Number);
+const map = input.slice(1).map((line) => line.split(''));
+const visited = Array.from({ length: R }, () => Array(C).fill(false));
+
+let totalSheep = 0;
+let totalWolves = 0;
+
+const directions = [
+  [-1, 0], // 상
+  [1, 0], // 하
+  [0, -1], // 좌
+  [0, 1], // 우우
+];
+
+const bfs = (x, y) => {
+  let queue = [[x, y]];
+  visited[x][y] = true;
+
+  let sheep = 0;
+  let wolves = 0;
+
+  if (map[x][y] === 'k') sheep++;
+  if (map[x][y] === 'v') wolves++;
+
+  while (queue.length) {
+    const [qx, qy] = queue.shift();
+
+    for (const [dx, dy] of directions) {
+      const nx = qx + dx;
+      const ny = qy + dy;
+
+      if (
+        nx >= 0 &&
+        nx < R &&
+        ny >= 0 &&
+        ny < C &&
+        !visited[nx][ny] &&
+        map[nx][ny] !== '#'
+      ) {
+        visited[nx][ny] = true;
+        queue.push([nx, ny]);
+
+        if (map[nx][ny] === 'k') sheep++;
+        if (map[nx][ny] === 'v') wolves++;
+      }
+    }
+  }
+
+  if (sheep > wolves) totalSheep += sheep;
+  else totalWolves += wolves;
+};
+
+for (let i = 0; i < R; i++) {
+  for (let j = 0; j < C; j++) {
+    if (map[i][j] !== '#' && !visited[i][j]) {
+      bfs(i, j);
+    }
+  }
+}
+
+console.log(totalSheep, totalWolves);
