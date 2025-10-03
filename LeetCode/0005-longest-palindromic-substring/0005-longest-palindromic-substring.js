dd82444553005deaaa46c57db94fb52b4202c16b
leegyuho-programer <101032270+leegyuho-programer@users.noreply.github.com>
2025-10-03 12:44:59 +0000

    chore: auto-move LeetCode problem folders to /LeetCode

--- LeetCode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ LeetCode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,13 +3,14 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    if(s.length < 2) return s;
-
     let start = 0;
     let maxLength = 1;
 
+    if(s.length < 2) return s;
+
     const expand = (l, r) => {
         while(l >= 0 && r < s.length && s[l] === s[r]) {
+            // 더 긴 팰린드롬을 찾으면 maxLength를 갱신
             if(r - l + 1 > maxLength) {
                 start = l;
                 maxLength = r - l + 1;
@@ -17,12 +18,12 @@ var longestPalindrome = function(s) {
             l--;
             r++;
         }
+    };
+
+    for(let i = 0; i < s.length; i++) {
+        expand(i, i);
+        expand(i, i+1);
     }
 
-    for (let i = 0; i < s.length; i++) {
-        expand(i, i);     // 홀수 길이 중심: 예) "aba"에서 b가 중심
-        expand(i, i + 1); // 짝수 길이 중심: 예) "abba"에서 중간의 두 b 사이
-    }
-
-    return s.substring(start, start + maxLength);
+    return s.substring(start, start + maxLength)
 };
