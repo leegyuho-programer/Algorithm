209e2150e396ce1206cc599021d994a057bfc369
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-10-07 01:13:06 +0900

    LeetHub | 0455-assign-cookies  (Easy) | Time: 257 ms (5.44%), Space: 63.5 MB (5.75%)

---
+++ 0455-assign-cookies/0455-assign-cookies-10-07-2025-01-13-05.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    const sortedG = g.sort((a,b) => a - b);
+    const sortedS = s.sort((a,b) => a - b);
+    let i = sortedS.length;
+    let j = sortedG.length;
+    let result = 0;
+
+    while(sortedS.length > 0) {
+        if(sortedS[0] >= sortedG[0]) {
+            sortedS.shift();
+            sortedG.shift();
+            result++;
+        } else {
+            sortedS.shift();
+        }
+    }
+    return result;
+};
