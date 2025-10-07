1bb7c7276b2ca083ed543adcaf73e9e2a44b5c50
LEE GYUHO <101032270+leegyuho-programer@users.noreply.github.com>
2025-10-07 17:23:05 +0900

    LeetHub | 0860-lemonade-change  (Easy) | Time: 2 ms (78.44%), Space: 63.2 MB (94.93%)

---
+++ 0860-lemonade-change/0860-lemonade-change-10-07-2025-17-23-05.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let five = 0;
+    let ten = 0;
+    let twenty = 0;
+
+    for(let i = 0; i < bills.length; i++) {
+        if(bills[i] === 5) {
+            five++;
+        } else if(bills[i] === 10) {
+            ten++;
+            five--;
+        } else {
+            if(ten > 0) {
+                ten--;
+                five--;
+            } else {
+                five -= 3;
+            }
+        }
+
+        if(ten < 0 || five < 0) {
+            return false;
+        }
+    }
+    return true;
+};
